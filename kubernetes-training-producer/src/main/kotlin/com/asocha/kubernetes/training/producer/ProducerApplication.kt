717ac858481ab9107df68276a6686f4aef1f26e6
arkadiusz <socha.arkadiusz@gmail.com>
2020-11-21 15:47:09 +0100

    Producer correctly builded to fat-jar

--- kubernetes-training-producer/src/main/kotlin/com/asocha/kubernetes/training/producer/ProducerApplication.kt
+++ kubernetes-training-producer/src/main/kotlin/com/asocha/kubernetes/training/producer/ProducerApplication.kt
@@ -1,11 +1,11 @@
 package com.asocha.kubernetes.training.producer
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class ProducerApplication
 
 fun main(vararg args: String) {
-    SpringApplication(ProducerApplication::class.java).run(*args)
+    runApplication<ProducerApplication>(*args)
 }
